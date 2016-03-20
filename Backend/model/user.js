b660a9cae0fdd792744730e75ad91963f840e698
kate <lobanova.e.o@gmail.com>
2016-03-20 14:18:21 +0100

    Created authentication model

---
+++ Backend/model/user.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose'),
+	config = require('./config');
+
+mongoose.connect(config.get('mongoose:uri'));
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('Connection error:', err.message);
+});
+db.once('open', function callback () {
+    console.log("Connected to database.");
+});
+
+module.exports = mongoose.model('UsersModel',{
+	id: String,
+    role: {
+        type: String,
+        enum: config.get('roles'),
+        required: true
+    },
+	username: { type: String, required: true, unique: true},
+    email: { type: String, required: true},
+	password: { type: String, required: true}
+});
