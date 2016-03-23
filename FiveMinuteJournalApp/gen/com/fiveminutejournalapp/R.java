057f7efba1f50933c53b987d8c928d65a7fc12b5
kate <lobanova.e.o@gmail.com>
2016-03-23 16:48:46 +0100

    Customized the font and colors

--- FiveMinuteJournalApp/gen/com/fiveminutejournalapp/R.java
+++ FiveMinuteJournalApp/gen/com/fiveminutejournalapp/R.java
@@ -10,6 +10,10 @@ package com.fiveminutejournalapp;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bordo=0x7f040000;
+        public static final int white=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,31 +22,32 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int welcome_text_size=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int open_journal_size=0x7f050003;
+        public static final int welcome_text_size=0x7f050002;
     }
     public static final class drawable {
         public static final int canvas=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int open_journal_button=0x7f080001;
-        public static final int welcomeText=0x7f080000;
+        public static final int action_settings=0x7f090002;
+        public static final int open_journal_button=0x7f090001;
+        public static final int welcomeText=0x7f090000;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
+        public static final int home=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050004;
-        public static final int app_name=0x7f050000;
-        public static final int home_label=0x7f050001;
-        public static final int open_journal_button=0x7f050003;
-        public static final int welcome_text=0x7f050002;
+        public static final int action_settings=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int home_label=0x7f060001;
+        public static final int open_journal_button=0x7f060003;
+        public static final int welcome_text=0x7f060002;
     }
     public static final class style {
         /** 
@@ -66,10 +71,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- FiveMinuteJournalApp/src/com/fiveminutejournalapp/HomeActivity.java
+++ FiveMinuteJournalApp/src/com/fiveminutejournalapp/HomeActivity.java
@@ -3,18 +3,26 @@ package com.fiveminutejournalapp;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeActivity extends Activity {
 	private TextView welcomeTextView;
+	private Button openJournalButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		
+
 		welcomeTextView = (TextView) findViewById(R.id.welcomeText);
-        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/Raleway-Regular.ttf");
-        welcomeTextView.setTypeface(face);
+		Typeface ralewayFace = Typeface.createFromAsset(getAssets(),
+				"fonts/Raleway-Regular.ttf");
+		welcomeTextView.setTypeface(ralewayFace);
+
+		openJournalButton = (Button) findViewById(R.id.open_journal_button);
+		Typeface gothamFace = Typeface.createFromAsset(getAssets(),
+				"fonts/gotham-bold.ttf");
+		openJournalButton.setTypeface(gothamFace);
 	}
 }
